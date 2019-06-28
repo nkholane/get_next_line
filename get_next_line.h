9522f316efdf1619b125b635aee15060527cd77d
Nokhwezi KHOLANE <nkholane@c4r12s6.wethinkcode.co.za>
2019-06-28 16:36:19 +0200

    first push

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkholane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/28 16:08:30 by nkholane          #+#    #+#             */
+/*   Updated: 2019/06/28 16:35:53 by nkholane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "libft.h"
+ 
+int get_next_line(const int fd, char **line);
+
+#endif
