f4dd96d9a7185ac57a2090f302bdc5386933e12e
Annunaki Goddess <50703910+nkholane@users.noreply.github.com>
2019-06-30 17:27:06 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -10,4 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+int get_next_line(int const fd, char **line)
+{
+    char buffer[2] = "";
+
+    /* Allocate line memory if needed */
+    if( !*line )
+        *line = malloc(100 * sizeof(char));
+
+    /* Blank out the line */
+    *line[0] = '\0';
+
+    while( read(fd, buffer, 1) > 0 ) {
+        strncat(*line, buffer, 1);
+        if( buffer[0] == '\n' )
+            break;
+    }
+
+    return 0;
+}
